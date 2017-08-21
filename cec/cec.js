2d1b06b9db26078ba46958fd2f9170d63cb88c04
Damien Clark <damo.clarky@gmail.com>
2017-08-21 13:58:07 +1000

    Added class to manage multiple shared cec connections between cec nodes

--- cec/cec.js
+++ cec/cec.js
@@ -30,6 +30,40 @@ var c = require('@senzil/cec-monitor') ;
 var CEC = c.CEC ;
 var CECMonitor = c.CECMonitor ;
 var RED = null ;
+
+function MonManager() {
+	this.state = {} ;
+}
+
+MonManager.prototype.init = function(config) {
+	var cec_adapter = RED.nodes.getNode(config.cec_adapter);
+	if(!this.state.hasOwnProperty(config.cec_adapter)) {
+		this.state[config.cec_adapter] = new CECMonitor(cec_adapter.OSDname, cec_adapter) ;
+	}
+	cec_adapter.debug = false ;
+	cec_adapter.processManaged = true ;
+	cec_adapter.autorestart = true ;
+	cec_adapter.no_serial = {//controls if the monitor restart cec-client when that stop after the usb was unplugged
+		reconnect: true,       //enable reconnection attempts when usb is unplugged
+		wait_time: 30,          //in seconds - time to do the attempt
+		trigger_stop: true     //avoid trigger stop event
+	} ;
+	return cec_adapter ;
+} ;
+
+MonManager.prototype.get = function(cec_adapter) {
+	return this.state[cec_adapter] ;
+} ;
+
+MonManager.prototype.delete = function (cec_adapter) {
+	if(this.state.hasOwnProperty(cec_adapter)) {
+		this.state[cec_adapter].Stop() ;
+		delete this.state[cec_adapter] ;
+	}
+} ;
+
+var mon = new MonManager() ;
+
 var state = {} ;
 
 module.exports = {
@@ -46,16 +80,11 @@ module.exports = {
 		var node = this ;
 		RED.nodes.createNode(node,config) ;
 
+		node.status({fill:"grey",shape:"ring",text:"Connecting"}) ;
+
 		// Retrieve the config node
-		node.cec_adapter = RED.nodes.getNode(config.cec_adapter);
-		node.cec_adapter.debug = false ;
-		node.cec_adapter.processManaged = true ;
-		node.cec_adapter.autorestart = true ;
-		node.cec_adapter.no_serial = {//controls if the monitor restart cec-client when that stop after the usb was unplugged
-			reconnect: true,       //enable reconnection attempts when usb is unplugged
-			wait_time: 30,          //in seconds - time to do the attempt
-			trigger_stop: true     //avoid trigger stop event
-		} ;
+		node.cec_adapter = mon.init(config) ;
+		var monitor = mon.get(config.cec_adapter) ;
 		node.config = config ;
 		node.config.select_all = (node.config.select_all == 'true') ;
 
@@ -66,36 +95,25 @@ module.exports = {
 			} else {
 				// This node is being restarted
 			}
-			monitor.Stop() ;
-			if(state.hasOwnProperty(config.cec_adapter))
-				delete state[config.cec_adapter] ;
+			mon.delete(config.cec_adapter) ;
 			done() ;
 		});
-
-		var monitor ;
-		if(state.hasOwnProperty(config.cec_adapter)) {
-			monitor = state[config.cec_adapter] ;
-		}
-		else {
-			monitor = new CECMonitor(node.cec_adapter.OSDname, node.cec_adapter) ;
-			state[config.cec_adapter] = monitor ;
-		}
-
-		node.status({fill:"grey",shape:"ring",text:"Connecting"}) ;
 
 		var ready = function() {
 			node.warn("Node config:"+JSON.stringify(node.config)) ;
 			node.status({fill:"green",shape:"dot",text:"Connected"}) ;
 		} ;
 		monitor.once(CECMonitor.EVENTS._READY, ready) ;
+
 		monitor.on(CECMonitor.EVENTS._STOP,function () {
 			node.status({fill:"red",shape:"ring",text:"Disconnected"}) ;
 			monitor.once(CECMonitor.EVENTS._READY, ready) ;
 		}) ;
 
 		var send = function(packet) {
-			if(packet.flow === "IN" && node.config.flow_in === true
-				|| packet.flow === "OUT" && node.config.flow_out === true) {
+			var isAllowedIn = (packet.flow === "IN" && node.config.flow_in === true) ;
+			var isAllowedOut = (packet.flow === "OUT" && node.config.flow_out === true) ;
+			if(isAllowedIn || isAllowedOut) {
 				node.send({payload: packet}) ;
 			}
 		} ;
